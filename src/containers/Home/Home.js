091ede482019e5d88436c64abd0dc838a2b15589
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-03-05 23:59:09 +0300

    set card to home container

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 
 import User from '../../components/User/User';
+import Card from '../../components/UIElements/Card/Card';
+import Input from '../../components/FormElements/Input/Input';
+import Button from '../../components/FormElements/Button/Button';
 import './Home.css';
 
 const Home = props => {
@@ -9,8 +12,13 @@ const Home = props => {
         <div className='home-left'>
             <User/>
         </div>
-        <div>
-
+        <div className='home-right'>
+            <Card>
+                <div className='card-item create-card-item'>
+                    <p className='card-title'>New Project</p>
+                    <Button className='outline'>Create</Button>
+                </div>
+            </Card>
         </div>
     </div>
 };
