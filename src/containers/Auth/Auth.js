b8153f5ce704503da2ab16537f609e1c042491b9
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-03-05 23:06:44 +0300

    set auth container dynamically

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import validator from 'validator';
 
 import Input from '../../components/FormElements/Input/Input';
 import Button from '../../components/FormElements/Button/Button';
@@ -6,13 +8,37 @@ import Card from '../../components/UIElements/Card/Card';
 import './Auth.css';
 
 const Auth = props => {
+    const history = useHistory();
+    const [ userName, setUserName ] = useState('');
+    const [ error, setError ] = useState(false);
+
+    const inputHandler = event => {
+        setUserName(event.target.value);
+        if(error) setError(false);
+    };
+
+    const login = event => {
+        event.preventDefault();
+        if(validator.isLength(userName,{ min: 2 })){
+            localStorage.setItem('userName', userName);
+            history.push('/');
+        } else setError(true);
+    };
+
     return <div className='auth'>
         <Card>
-            <div className='auth-form-container'>
+            <form onSubmit={login} className='auth-form-container'>
                 <p className='auth-title'>Name</p>
-                <Input className='mb-1' placeholder='enter your full name...' element='input'/>
-                <Button>Login</Button>
-            </div>
+                <Input 
+                    value={userName}
+                    onChange={inputHandler} 
+                    className='mb-1'
+                    placeholder='enter your full name...' 
+                    element='input'
+                />
+                {error && <p className='error-text mb-1'>Please enter your name</p>}
+                <Button onClick={login}>Login</Button>
+            </form>
         </Card>
     </div>
 };
