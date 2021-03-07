7b249ca1533e56d292155876e1edf150ff1ecace
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-03-07 13:29:44 +0300

    edit input component

--- src/components/FormElements/Input/Input.js
+++ src/components/FormElements/Input/Input.js
@@ -6,6 +6,17 @@ import './Input.css';
 const Input = props => {
     let element;
     switch(props.element){
+        case 'checkbox': element = <label class="label">{props.label}
+            <input
+                className={`input ${props.className}`} 
+                type={props.type} 
+                value={props.value} 
+                placeholder={props.placeholder} 
+                onChange={props.onChange}
+            />
+            <span class="checkmark"></span>
+        </label>;
+        break;
         case 'input': element = <input
             className={`input ${props.className}`} 
             type={props.type} 
